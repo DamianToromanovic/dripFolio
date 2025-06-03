bd6bfc0aca396d1e611990d5062fbc8d86fcfee6
DamianToromanovic <d.toromanovic@web.de>
2025-06-03 12:05:33 +0200

    grid 2xl => 10 cols

--- code/src/App.jsx
+++ code/src/App.jsx
@@ -5,12 +5,12 @@ import Footer from "./components/Footer";
 export default function App() {
   return (
     <main className="min-h-screen w-full bg-zinc-950 text-zinc-200 transition-colors duration-300">
-      <div className=" mx-auto px-4 py-12 grid grid-cols-1 2xl:grid-cols-4 gap-6">
-        <div className="lg:px-10 xl:px-14  2xl:px-4">
+      <div className=" mx-auto px-4 py-12 grid grid-cols-1 2xl:grid-cols-10 gap-6">
+        <div className="lg:px-10 xl:px-14  2xl:px-4 2xl:col-span-3">
           <Hero />
         </div>
 
-        <div className="lg:px-10 xl:px-14 2xl:col-span-3 2xl:px-4">
+        <div className="lg:px-10 xl:px-14 2xl:col-span-7 2xl:px-4">
           <SummaryCard />
         </div>
       </div>
--- code/src/components/Hero.jsx
+++ code/src/components/Hero.jsx
@@ -1,5 +1,5 @@
-import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { useRef, useState, useEffect } from "react";
+import { Github, Mail, MapPin } from "lucide-react";
 
 export default function Hero() {
   const heroRef = useRef(null);
@@ -23,10 +23,10 @@ export default function Hero() {
       ref={heroRef}
       className="w-full  bg-zinc-950 text-zinc-200 transition-colors duration-500"
     >
-      <div className=" mx-auto border border-zinc-700 rounded-xl shadow-lg p-6 bg-zinc-900 text-zinc-200 relative">
+      <div className=" mx-auto border border-zinc-700 rounded-2xl shadow-lg p-6 bg-[#1e1e1f] text-zinc-200 relative">
         <div className="flex items-start justify-between">
           <div className="flex gap-4 items-center">
-            <div className="w-32 h-32 overflow-hidden rounded-2xl shrink-0  bg-zinc-800">
+            <div className="w-32 h-32 overflow-hidden rounded-[20%] shrink-0  bg-zinc-800">
               <img
                 src="/assets/sticker.png"
                 className="w-full object-cover"
@@ -34,8 +34,8 @@ export default function Hero() {
               />
             </div>
             <div className="">
-              <h1 className="text-xl font-bold ">Damian Toromanovic</h1>
-              <p className="text-sm text-zinc-400 bg-zinc-800 py-1 px-2 inline-block rounded-lg mt-2">
+              <h1 className="text-3xl font-bold ">Damian Toromanovic</h1>
+              <p className="text-sm text-zinc-200 bg-[#2b2b2c] py-1 px-2 inline-block rounded-lg mt-4">
                 FullStack Developer
               </p>
             </div>
@@ -44,27 +44,46 @@ export default function Hero() {
 
         {showMore && (
           <div className="mt-4 text-sm space-y-2">
-            <p>📍 Location: Germany</p>
-            <p>🎂 Age: {new Date().getFullYear() - 1999}</p>
-            <p>✉️ Email: d.toromanovic@web.de</p>
             <hr className="border-zinc-700" />
-            <div className="flex gap-4 pt-1">
-              <a
-                href="https://github.com/DamianToromanovic"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-yellow-500 hover:underline"
-              >
-                <FaGithub className="inline" /> GitHub
-              </a>
-              <a
-                href="https://linkedin.com/in/DEINNAME"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-yellow-500 hover:underline"
-              >
-                <FaLinkedin className="inline" /> LinkedIn
-              </a>
+            <div className="flex gap-4 items-center">
+              <div className="p-3 bg-[#202022] rounded-xl">
+                {" "}
+                <Mail className="text-[#DAA520]" />
+              </div>
+
+              <div className="flex flex-col">
+                <span className="text-zinc-600">EMAIL</span>
+                <span className="text-lg">d.toromanovic@web.de</span>
+              </div>
+            </div>
+
+            <div className="flex gap-4 items-center">
+              <div className="p-3 bg-[#202022] rounded-xl">
+                {" "}
+                <MapPin className="text-[#DAA520]" />
+              </div>
+
+              <div className="flex flex-col">
+                <span className="text-zinc-600">Location</span>
+                <span className="text-lg">Germany</span>
+              </div>
+            </div>
+
+            <div className="flex gap-4 items-center">
+              <div className="p-3 bg-[#202022] rounded-xl">
+                <Github className="text-[#DAA520]" />
+              </div>
+              <div className="flex flex-col">
+                <span className="text-zinc-600">GitHub</span>
+                <a
+                  href="https://github.com/DamianToromanovic"
+                  className="text-lg  hover:underline"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  View Profile
+                </a>
+              </div>
             </div>
           </div>
         )}
