f02b81635969fed6fe928405cd334773ab5ddf5c
DamianToromanovic <d.toromanovic@web.de>
2025-06-07 21:21:19 +0200

    portfolio ready

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import Footer from "./components/Footer";
 
 export default function App() {
   return (
-    <main className="min-h-screen w-full bg-my-background text-zinc-200 transition-colors duration-300 ">
+    <main className="min-h-screen w-full bg-my-background text-my-foreground transition-colors duration-300 ">
       <div className=" mx-auto px-4 py-12 grid grid-cols-1 3xl:grid-cols-10 gap-6 ">
-        <div className="lg:px-20  xl:px-24 3xl:px-4 3xl:col-span-2 3xl:col-start-2">
+        <div className="lg:px-20 xl:px-24 3xl:px-4 3xl:col-span-2 3xl:col-start-2 ">
           <Hero />
         </div>
 
-        <div className="lg:px-20 xl:px-24 3xl:col-span-6 3xl:px-12 ">
+        <div className="lg:px-20  xl:px-24 3xl:col-span-6 3xl:px-12 ">
           <SummaryCard />
         </div>
       </div>
